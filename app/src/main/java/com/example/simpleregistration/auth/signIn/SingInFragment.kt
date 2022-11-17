ab2eba1afafcf18ffa7f974ad56fbb66166901b3
Murad <code.exe.machine1337@gmail.com>
2022-11-17 20:55:59 +0300

    delete picture meme

--- app/src/main/java/com/example/simpleregistration/auth/signIn/SingInFragment.kt
+++ app/src/main/java/com/example/simpleregistration/auth/signIn/SingInFragment.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.simpleregistration.R
 import com.example.simpleregistration.auth.UserRole
@@ -17,15 +16,6 @@ class SingInFragment : Fragment(R.layout.fragment_sign_in) {
 
     private lateinit var binding: FragmentSignInBinding
     private val viewModel by viewModels<AuthViewModel> { AuthViewModelFactory() }
-
-//    private val navOptions by lazy {
-//        NavOptions.Builder()
-//            .setLaunchSingleTop(true)
-//            .setEnterAnim(R.anim.)
-//            .setExitAnim(R.anim.slide_out_left)
-//            .setPopEnterAnim(R.anim.slide_in_left)
-//            .setPopExitAnim(R.anim.slide_out_right)
-//    }
 
     override fun onStart() {
         super.onStart()
